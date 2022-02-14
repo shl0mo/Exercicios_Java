db9452cd6cfbd9761c08149d011ab8ff4be7c12c
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-02-14 17:07:24 -0400

    init

---
+++ lista_2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+
+	static class Q1 {
+		public void imprime_vetor() {
+			
+		}
+
+		Scanner sc = new Scanner(System.in);
+		int vetor[] = new int[10];
+		double vetor_resposta[] new double[10];
+		int i = 0;
+		for (i = 0; i < 10; i++) {
+			vetor[i] = 
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java!");
+	}
+}
