8bf14dc9bca21a27d062cfd645fb1c1e893c5b66
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-03-03 18:37:49 -0400

    Encapsulamento [...] - Classe Pairing

--- Encapsulamento, modificadores de acesso e sobreposições/Q1.java
+++ Encapsulamento, modificadores de acesso e sobreposições/Q1.java
@@ -1,5 +1,6 @@
 class Main {
-
+	
+	//Q1:
 	static class Pair {
 		private String left;
 		private String right;
@@ -22,10 +23,50 @@ class Main {
 			}
 		}
 	}
+	
+	//Q2:
+	static class Pairing {
+		private Pair pairs[];
+                
+                Pairing (String s1, String s2) {
+                    try {
+                        String array_s1[] = getList(s1);
+                        String array_s2[] = getList(s2);
+                        if (array_s1.length != array_s2.length) {
+                            throw IncompatibleArgumentsExceptions();
+                        }
+                    } catch (IncompatibleArgumentsExceptions) {
+                        System.out.println("\nArrays com diferentes tamanhos");
+                    }
+                    this.pairs = obtainPairs(s1, s2);
+		}
 
+		public Pair getPairs(int i) {
+			return this.pairs[i];
+		}
+
+		private String[] getList(String s) {
+			return s.split(",");
+		}
+
+		public Pair[] obtainPairs (String s1, String s2) {
+			try {
+				String array_s1[] = getList(s1);
+				String array_s2[] = getList(s2);
+				Pair objPair[] = new Pair[array_s1.length];
+				for (int i = 0; i < array_s1.length; i++) {
+					Pair pair = new Pair(array_s1[i], array_s2[i]);
+					objPair[i] = pair;
+				}
+				return objPair;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+	
 	public static void main(String args[]) {
-		Pair objPair = new Pair("Esquerda", "Direita");
-		Pair objPair2 = new Pair("Esquerda", "Direita");
-		System.out.println(objPair.equals(objPair, objPair2));
+            Pairing pairing = new Pairing("Teste 1", "Teste 2");
 	}
 }
