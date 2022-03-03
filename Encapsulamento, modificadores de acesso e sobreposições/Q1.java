8ef7d445c9541a95591cd2166e4cd5d24123ad72
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-03-03 02:10:55 -0400

    Q1 - sobreposição toString()

--- Encapsulamento, modificadores de acesso e sobreposições/Q1.java
+++ Encapsulamento, modificadores de acesso e sobreposições/Q1.java
@@ -1,9 +1,18 @@
 class Main {
 
 	static class Pair {
-		private String letft;
+		private String left;
 		private String right;
 
+		Pair (String left, String right) {
+			this.left = left;
+			this.right = right;
+		}
+		
+		@Override
+		public String toString() {
+			return "(" + this.left + ", " + this.right + ")";
+		}
 	}
 
 	public static void main(String args[]) {
