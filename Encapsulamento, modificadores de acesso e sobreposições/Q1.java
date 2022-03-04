8ec7354ad0c3b717695ffcbdc3c2fea9f445a92e
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-03-04 02:54:02 -0400

    Encapsulamento [...] - Classe Pairing - final

--- Encapsulamento, modificadores de acesso e sobreposições/Q1.java
+++ Encapsulamento, modificadores de acesso e sobreposições/Q1.java
@@ -1,72 +1,83 @@
+import java.util.Arrays;
+
 class Main {
-	
-	//Q1:
-	static class Pair {
-		private String left;
-		private String right;
 
-		Pair (String left, String right) {
-			this.left = left;
-			this.right = right;
-		}
-		
-		@Override
-		public String toString() {
-			return "(" + this.left + ", " + this.right + ")";
-		}
+	class Pair {
+	    private String left;
+	    private String right;
+	    
+	    Pair (String left, String right) {
+		this.left = left;
+	    	this.right = right;
+	    }
+			
+	    @Override
+	    public String toString() {
+	    	return "(" + this.left + ", " + this.right + ")";
+	    }
 
-		public boolean equals(Pair p1, Pair p2) {
-			if (p1.left.equals(p2.left) && p1.right.equals(p2.right)) {
-				return true;
-			} else {
-				return false;
-			}
+	    public boolean equals(Object o) {
+		if (this.left.equals(((Pair)o).left) && this.right.equals(((Pair)o).right)) {
+			return true;
+		} else {
+			return false;
 		}
+	    }
 	}
 	
-	//Q2:
 	static class Pairing {
-		private Pair pairs[];
-                
-                Pairing (String s1, String s2) {
-                    try {
-                        String array_s1[] = getList(s1);
-                        String array_s2[] = getList(s2);
-                        if (array_s1.length != array_s2.length) {
-				throw new Exception();
-			}
-                    } catch (Exception e) {
-                        System.out.println("Arrays com diferentes tamanhos");
-                    }
-                    this.pairs = obtainPairs(s1, s2);
+	    private Pair pairs[];
+		       
+	    Pairing (String s1, String s2) throws Exception {
+		String array_s1[] = getList(s1);
+		String array_s2[] = getList(s2);
+		if (array_s1.length != array_s2.length) {
+		    throw new Exception("Cannot obtain pairs with different sizes!");
 		}
+		if (s1.equals("") || s2.equals("")) {
+		    this.pairs = new Pair[0];
+		} else if (getList(s1).length == 1) {
+		    Pair array_pairs[] = new Pair[1];
+		    array_pairs[0] = new Pair(s1, s2); 
+		    this.pairs = array_pairs;
+		} else {
+		    this.pairs = obtainPairs(s1, s2);
+		}
+	    }
 
-		public Pair getPairs(int i) {
-			return this.pairs[i];
-		}
+	    public Pair[] getPairs() {
+		return this.pairs;
+	    }
 
-		private String[] getList(String s) {
-			return s.split(",");
-		}
+	    private String[] getList(String s) {
+		return s.split(", ");
+	    }
 
-		public Pair[] obtainPairs (String s1, String s2) {
-			try {
-				String array_s1[] = getList(s1);
-				String array_s2[] = getList(s2);
-				Pair objPair[] = new Pair[array_s1.length];
-				for (int i = 0; i < array_s1.length; i++) {
-					Pair pair = new Pair(array_s1[i], array_s2[i]);
-					objPair[i] = pair;
-				}
-				return objPair;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return null;
+	    public Pair[] obtainPairs (String s1, String s2) throws Exception {
+		String array_s1[] = getList(s1);
+		String array_s2[] = getList(s2);
+		if (array_s1.length != array_s2.length) {
+		    throw new Exception("Cannot obtain pairs with different sizes!");
 		}
+		Pair objPair[] = new Pair[array_s1.length];
+		    for (int i = 0; i < array_s1.length; i++) {
+		        Pair pair = new Pair(array_s1[i], array_s2[i]);
+		        objPair[i] = pair;
+		    }
+		return objPair;
+	    }
+	    
+	    @Override
+	    public String toString() {
+		return Arrays.toString(this.pairs).replace("[","").replace("]","");
+	    }
 	}
-	
-	public static void main(String args[]) {
-            Pairing pairing = new Pairing("Teste, 1", "Teste, 2, 3");
-	}
+		
+        public static void main(String args[]) {
+	    String s1 = "Person 1, Person 3, Person 5";
+	    String s2 = "Person 2, Person 4, Person 6";
+            Pairing pairing = new Pairing(s1, s2);
+            //String resultado = pairing.pairs.toString();
+            System.out.println(pairing.getPairs(0).toString());
+        }
 }
