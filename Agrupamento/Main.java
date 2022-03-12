45b074bb06fa87edfc49e3f2fdbd63145666260e
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-03-12 00:36:01 -0400

    Agrupamento - Suite, Hospede - getters e setters

--- Agrupamento/Main.java
+++ Agrupamento/Main.java
@@ -16,19 +16,88 @@ public class Main {
 	    }
 	}
 
-	static class Hospede {
-		private int codigo;
-		private String nome;
-		private String endereco;
-		private int idade;
+	public class Hospede {
+	    
+	    private int codigo;
+	    private String nome;
+	    private String endereco;
+	    private int idade;
+	    
+	    public int getCodigo () {
+		return this.codigo;
+	    }
+	    
+	    public String getNome () {
+		return this.nome;
+	    }
+	    
+	    public String getEndereco () {
+		return this.endereco;
+	    }
+	    
+	    public int getIdade () {
+		return this.idade;
+	    }
+	    
+	    public void setCodigo (int codigo) {
+		this.codigo = codigo;
+	    }
+	    
+	    public void setNome (String nome) {
+		this.nome = nome;
+	    }
+	    
+	    public void setEndereco (String endereco) {
+		this.endereco = endereco;
+	    }
+	    
+	    public void setIdade (int idade) {
+		this.idade = idade;
+	    }
+	    
 	}
 	
 	public class Suite {
+	    
 	    private int numero;
 	    private String tipo;
 	    private int capacidade;
 	    private double valorDiaria;
+	    
+	    public int getNumero () {
+		return this.numero;
+	    }
+	    
+	    public String getTipo () {
+		return this.tipo;
+	    }
+	    
+	    public int getCapacidade () {
+		return this.capacidade;
+	    }
+	    
+	    public double getValorDiaria() {
+		return this.valorDiaria;
+	    }
+	    
+	    public void getNumero (int numero) {
+		this.numero = numero;
+	    }
+	    
+	    public void setTipo (String tipo) {
+		this.tipo = tipo;
+	    }
+	    
+	    public void setCapacidade (int capacidade) {
+		this.capacidade = capacidade;
+	    }
+	    
+	    public void setValorDiaria (double valorDiaria) {
+		this.valorDiaria = valorDiaria;
+	    }
+	    
 	}
+
 	
 	public static void main(String[] args) {
         	System.out.println("Olá, mundo!")
