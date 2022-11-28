ba7a5ea458f643fdf4be98b5786005d427d0b2a8
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-11-28 17:26:05 -0400

    feat(PP) - Lab-06 - Retangulo.java

---
+++ PP/Lab-06/Retangulo.java
@@ -0,0 +1,27 @@
+package br.edu.icomp.ufam.lab_heranca;
+
+public class Retangulo extends FormaGeometrica {
+	public double largura;
+	public double altura;
+
+	public Retangulo (int posX, int posY, double largura, double altura) {
+		super(posX, posY);
+		this.largura = largura;
+		this.altura = altura;
+	}
+	
+	@Override
+	public double getArea () {
+		return this.largura*this.altura;
+	}
+	
+	@Override
+	public double getPerimetro () {
+		return 2*(this.largura + this.altura);
+	}
+
+	@Override
+	public String toString () {
+		return "Retângulo na posição (" + this.posX + ", " + this.posY + ") com largura de " + this.largura + "cm e altura de " + this.altura + "cm (área=" + getArea() + "cm2, perímetro=" + getPerimetro() + "cm)";
+	}
+}
