f760cc1a6c8d4b28ceee066e1dceaa15337754cd
shl0mo <salomao.dacostacruz@gmail.com>
2023-01-05 22:52:46 -0400

    feat(Lab10) - SnakeJogador - 2

--- PP/Lab-10/SnakeJogador.java
+++ PP/Lab-10/SnakeJogador.java
@@ -14,6 +14,9 @@ public class SnakeJogador {
         boolean baixo_livre = jogo.isCelulaLivre(cabeca.x, cabeca.y+1);
         boolean esquerda_livre = jogo.isCelulaLivre(cabeca.x-1, cabeca.y);
 
+
+        
+
         while (cima_livre || direita_livre || baixo_livre || esquerda_livre) {
             cabeca = jogo.getSegmentos().getFirst();
             Point comida = this.jogo.getComida();
@@ -27,27 +30,87 @@ public class SnakeJogador {
             boolean comida_horizontal = Math.abs((cabeca.y + 1) - y_comida) == Math.abs((cabeca.y - 1) - y_comida);
 
             if (comida_vertical) {
-                if (comida_acima) return 'C';
-                else if (comida_abaixo) return 'B'; 
+                if (comida_acima) {
+                    if (cima_livre) {
+                        return 'C';
+                    } else {
+                        if (direita_livre) return 'D';
+                        else if (esquerda_livre) return 'E';
+                        else if (baixo_livre) return 'B';
+                    }
+                } else if (comida_abaixo) {
+                    if (baixo_livre) {
+                        return 'B';
+                    } else {
+                        if (direita_livre) return 'D';
+                        else if (esquerda_livre) return 'E';
+                        else if (cima_livre) return 'C';
+                    }
+                } 
             }
 
             if (comida_horizontal) {
-                if (comida_direita) return 'D';
-                else if (comida_esquerda) return 'E';
+                if (comida_direita) {
+                    if (direita_livre) {
+                        return 'D';
+                    } else {
+                        if (cima_livre) return 'C';
+                        else if (baixo_livre) return 'B';
+                        else if (esquerda_livre) return 'E';
+                    }
+                } else if (comida_esquerda) {
+                    if (esquerda_livre) {
+                        return 'E';
+                    } else {
+                        if (cima_livre) return 'C';
+                        else if (baixo_livre) return 'B';
+                        else if (esquerda_livre) return 'E';
+                    }
+                }
             }
 
-            if (comida_acima) return 'C';
+            if (comida_acima) {
+                int distancia_acima = Math.abs(cabeca.y - y_comida);
+                int distancia_direita = Math.abs((cabeca.x + 1) - x_comida);
+                int distancia_esquerda = Math.abs((cabeca.x - 1) - x_comida);
+                if (distancia_acima < distancia_direita && distancia_acima < distancia_esquerda) return 'C';
+                else if (distancia_direita < distancia_acima && distancia_direita < distancia_esquerda) return 'D';
+                else if (distancia_esquerda < distancia_acima && distancia_esquerda <  distancia_direita) return 'E';
+            }
 
-            if (comida_abaixo) return 'B';
+            if (comida_abaixo) {
+                int distancia_acima = Math.abs(cabeca.y - y_comida);
+                int distancia_direita = Math.abs((cabeca.x + 1) - x_comida);
+                int distancia_esquerda = Math.abs((cabeca.x - 1) - x_comida);
+                if (distancia_acima < distancia_direita && distancia_acima < distancia_esquerda) return 'C';
+                else if (distancia_direita < distancia_acima && distancia_direita < distancia_esquerda) return 'D';
+                else if (distancia_esquerda < distancia_acima && distancia_esquerda <  distancia_direita) return 'E';
+            }
 
             cima_livre = jogo.isCelulaLivre(cabeca.x, cabeca.y-1);
             direita_livre = jogo.isCelulaLivre(cabeca.x+1, cabeca.y);
             baixo_livre = jogo.isCelulaLivre(cabeca.x, cabeca.y+1);
-            esqerda_livre = jogo.isCelulaLivre(cabeca.x-1, cabeca.y);
-        }        
+            esquerda_livre = jogo.isCelulaLivre(cabeca.x-1, cabeca.y);
+        }
+
+        ArrayList<Character> possiveisDirecoes = new ArrayList<Character>(4);
+        cabeca = jogo.getSegmentos().getFirst();
+        
+        // Adiciona as possíveis direções na lista
+        if (jogo.isCelulaLivre(cabeca.x, cabeca.y-1)) possiveisDirecoes.add('C'); // Cima
+        if (jogo.isCelulaLivre(cabeca.x+1, cabeca.y)) possiveisDirecoes.add('D'); // Direita
+        if (jogo.isCelulaLivre(cabeca.x, cabeca.y+1)) possiveisDirecoes.add('B'); // Baixo
+        if (jogo.isCelulaLivre(cabeca.x-1, cabeca.y)) possiveisDirecoes.add('E'); // Esquerda
         
         // Não existe mais nenhuma direção disponível
-        return 'N';
+        if (possiveisDirecoes.size() == 0) return 'N';
+        
+        // Pega um número aleatório entre 0 e o tamanho da lista e retorna a direção
+        int posicao = new Random().nextInt(possiveisDirecoes.size());
+        return possiveisDirecoes.get(posicao);
+        
+        // Não existe mais nenhuma direção disponível
+        //return 'N';
     }
     
 }
