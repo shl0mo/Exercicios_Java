1def5ec9a474fdc8c4e02023100bbbe5617d776b
shl0mo <salomao.dacostacruz@gmail.com>
2023-01-05 21:35:45 -0400

    feat(Lab10): SnakeJogador - 1

--- PP/Lab-10/SnakeJogador.java
+++ PP/Lab-10/SnakeJogador.java
@@ -12,31 +12,39 @@ public class SnakeJogador {
         boolean cima_livre = jogo.isCelulaLivre(cabeca.x, cabeca.y-1);
         boolean direita_livre = jogo.isCelulaLivre(cabeca.x+1, cabeca.y);
         boolean baixo_livre = jogo.isCelulaLivre(cabeca.x, cabeca.y+1);
-        boolean esqerda_livre = jogo.isCelulaLivre(cabeca.x-1, cabeca.y);
+        boolean esquerda_livre = jogo.isCelulaLivre(cabeca.x-1, cabeca.y);
 
         while (cima_livre || direita_livre || baixo_livre || esquerda_livre) {
             cabeca = jogo.getSegmentos().getFirst();
             Point comida = this.jogo.getComida();
-            int x_comida = comida.getX();
-            int y_comida = comida.getY();
-            boolean comida_esquerda = abs((cabeca.x - 1) - x_comida) < abs((cabeca.x + 1) - x_comida)
-            boolean comida_direita = abs((cabeca.x + 1) - x_comida) < abs((cabeca.x - 1) - x_comida)
-            boolean comida_vertical = abs((cabeca.x + 1) - x_comida) == abs((cabeca.x - 1) - x_comida)
-            boolean comida_cima = abs((cabeca.y - 1) - y_comida) < abs((cabeca.y + 1) - y_comida)
-            boolean comida_baixo = abs((cabeca.y + 1) - y_comida) < abs((cabeca.y - 1) - y_comida)
+            int x_comida = (int) comida.getX();
+            int y_comida = (int) comida.getY();
+            boolean comida_esquerda = Math.abs((cabeca.x - 1) - x_comida) < Math.abs((cabeca.x + 1) - x_comida);
+            boolean comida_direita = Math.abs((cabeca.x + 1) - x_comida) < Math.abs((cabeca.x - 1) - x_comida);
+            boolean comida_vertical = Math.abs((cabeca.x + 1) - x_comida) == Math.abs((cabeca.x - 1) - x_comida);
+            boolean comida_acima = Math.abs((cabeca.y - 1) - y_comida) < Math.abs((cabeca.y + 1) - y_comida);
+            boolean comida_abaixo = Math.abs((cabeca.y + 1) - y_comida) < Math.abs((cabeca.y - 1) - y_comida);
+            boolean comida_horizontal = Math.abs((cabeca.y + 1) - y_comida) == Math.abs((cabeca.y - 1) - y_comida);
 
-            if ()
+            if (comida_vertical) {
+                if (comida_acima) return 'C';
+                else if (comida_abaixo) return 'B'; 
+            }
+
+            if (comida_horizontal) {
+                if (comida_direita) return 'D';
+                else if (comida_esquerda) return 'E';
+            }
+
+            if (comida_acima) return 'C';
+
+            if (comida_abaixo) return 'B';
+
             cima_livre = jogo.isCelulaLivre(cabeca.x, cabeca.y-1);
             direita_livre = jogo.isCelulaLivre(cabeca.x+1, cabeca.y);
             baixo_livre = jogo.isCelulaLivre(cabeca.x, cabeca.y+1);
             esqerda_livre = jogo.isCelulaLivre(cabeca.x-1, cabeca.y);
-        }
-
-
-        if () return 'C'; // Cima
-        if () return 'D'; // Direita
-        if () return 'B'; // Baixo
-        if () return 'E'; // Esquerda
+        }        
         
         // Não existe mais nenhuma direção disponível
         return 'N';
